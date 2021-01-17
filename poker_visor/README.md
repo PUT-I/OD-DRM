ad04111aacdeaf647d08fb54046ceb6af390a0ba
travesom <travesom@gmail.com>
2021-01-17 08:55:27 +0100

    dd

---
+++ poker_visor/README.md
@@ -0,0 +1,16 @@
+# PokeVisor
+
+Program do rozpoznawania obrazu z gry w pokera oraz wizualizacji stanu gry na komputerze.
+Pisany jako projekt zaliczeniowy na Politechnice Poznańskiej.
+
+## Autorzy
+- Juliusz Horowski
+- Michał Kalinowski
+- Tomasz Kiljańczyk
+- Szymon Wasilewski
+
+## Dokumentacja projektowa
+Dokumentację można obejrzeć [tutaj](https://github.com/Gunock/PokeVisor/blob/master/Documentation_11_09_2020.pdf)
+
+## Prezentacja działania
+[![](http://img.youtube.com/vi/8kpK3PNAfo8/0.jpg)](http://www.youtube.com/watch?v=8kpK3PNAfo8 "")
---
+++ poker_visor/logic/card_detector/card_detection_example.py
@@ -0,0 +1,33 @@
+#
+# Based on: https://github.com/EdjeElectronics/OpenCV-Playing-Card-Detector
+#
+import argparse
+
+import cv2
+
+from logic.card_detector import card_detection
+
+
+def parse_arguments():
+    # construct argument parser and parse arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=False, help="path to image", default="cards_input.png")
+    return vars(ap.parse_args())
+
+
+def main():
+    args = parse_arguments()
+
+    image = cv2.imread(args["image"])
+    cards = card_detection.detect_cards(image, image)
+
+    print(cards)
+
+    # Finally, display the image with the identified cards
+    cv2.imshow("Card Detector", image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
