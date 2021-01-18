883b54af7fe4f3a96b74388bbbd950fa4e014ece
Gunock <asz.czyk@gmail.com>
2021-01-18 01:09:59 +0100

    Added docstring to unit tests

--- tests/test_key_utils.py
+++ tests/test_key_utils.py
@@ -1,3 +1,4 @@
+""" This script contains unit tests for class KeyUtils """
 import unittest
 
 from ddt import ddt
@@ -7,11 +8,34 @@ from serial_key_generator.key_utils import KeyUtils
 
 @ddt
 class TestKeyUtils(unittest.TestCase):
-    def test_get_key_byte(self):
-        seed = "test"
-        key_bytes = KeyUtils.get_key_bytes(seed, 1, 2, 3)
-        self.assertEqual("C0", key_bytes)
+    """ TestKeyUtils unit tests """
 
-    def test_get_checksum(self):
-        checksum = KeyUtils.get_checksum("test1234")
-        self.assertEqual("Z32G", checksum)
+    def test_get_key_byte_same_arguments(self):
+        """ Checks if get_key_bytes gives repeatable results for same arguments """
+
+        seed = "test"
+        key_bytes_1 = KeyUtils.get_key_bytes(seed, 1, 2, 3)
+        key_bytes_2 = KeyUtils.get_key_bytes(seed, 1, 2, 3)
+        self.assertEqual(key_bytes_1, key_bytes_2)
+
+    def test_get_key_byte_different_arguments(self):
+        """ Checks if get_key_bytes gives different results for different arguments """
+
+        key_bytes_1 = KeyUtils.get_key_bytes("test1", 1, 2, 3)
+        key_bytes_2 = KeyUtils.get_key_bytes("test2", 3, 2, 1)
+        self.assertNotEqual(key_bytes_1, key_bytes_2)
+
+    def test_get_checksum_same_arguments(self):
+        """ Checks if get_checksum gives repeatable results for same arguments """
+
+        input_str = "test1234"
+        checksum_1 = KeyUtils.get_checksum(input_str)
+        checksum_2 = KeyUtils.get_checksum(input_str)
+        self.assertEqual(checksum_1, checksum_2)
+
+    def test_get_checksum_different_arguments(self):
+        """ Checks if get_checksum gives different results for different arguments """
+
+        checksum_1 = KeyUtils.get_checksum("test1234")
+        checksum_2 = KeyUtils.get_checksum("test5678")
+        self.assertNotEqual(checksum_1, checksum_2)
