e2720341e7ead9b7c2bea4155a9554792210983b
Gunock <asz.czyk@gmail.com>
2021-01-23 10:50:04 +0100

    Create directory for application project

--- poke_visor/classes/card_detector/train_ranks.py
+++
@@ -1,9 +0,0 @@
-from poke_visor.enums.card_rank_enum import CardRank
-
-
-class TrainRanks:
-    """Structure to store information about train rank images."""
-
-    def __init__(self):
-        self.img = []  # Thresholded, sized rank image loaded from hard drive
-        self.rank = CardRank.unknown
--- poke_visor/classes/card_detector/train_suits.py
+++
@@ -1,9 +0,0 @@
-from poke_visor.enums.card_suit_enum import CardSuit
-
-
-class TrainSuits:
-    """Structure to store information about train suit images."""
-
-    def __init__(self):
-        self.img = []  # Thresholded, sized suit image loaded from hard drive
-        self.suit = CardSuit.unknown
